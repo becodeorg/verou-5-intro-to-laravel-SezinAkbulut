1751dc84f96533116c6b4cc8a8f04c9997a2aada
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-25 14:54:10 +0100

    connect laravel mix with js and sass

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,12 @@
     <title>@yield('title', 'Your App')</title>
     <!--Bootstrap-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!--Clockwork-->
     <script src="https://cdn.jsdelivr.net/gh/underground-works/clockwork-browser@1/dist/toolbar.js"></script>
-
+    <!--Laravel Mix JS-->
     <script src="{{ mix('js/app.js') }}"></script>
+    <!--Laravel Mix SASS-->
+    {{--<link rel="stylesheet" href="{{ mix('sass/app.scss') }}">--}}
 </head>
 <body>
 <header>
