c2877b497f65d63dc8fa77b94cf4983a1bb85bc3
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-25 16:13:51 +0100

    add register item in nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,9 @@
 <!-- views/partials/header.blade.php -->
 <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand text-white font-weight-bold text-success" href="{{ route('home') }}">Home</a>
+
+    <a class="nav-link" href="{{ route('register') }}">Register</a>
+
     <form class="form-inline" action="{{ route('search') }}" method="GET">
         <input class="form-control mr-sm-2 text-center" type="search" name="query" placeholder="Find a movie" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
