2f7b12c1244674447b02a9a0bc6cdf885904c04d
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-26 19:06:52 +0100

    update style of register page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,21 +1,19 @@
-<!-- nav bar-->
+<!-- nav bar -->
 <nav class="navbar navbar-dark bg-dark">
-
-    <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-        <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-            <li>
-                <a href="{{ route('show.home') }}" class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
+    <div class="d-flex flex-row align-items-center justify-content-between w-full">
+        <ul class="flex-row font-medium d-flex list-unstyled ml-5">
+            <li class="mr-4">
+                <a href="{{ route('show.home') }}" class="text-white">Home</a>
+            </li>
+            <li class="mr-4">
+                <a href="{{ route("showUsers") }}" class="text-white">Users</a>
             </li>
             <li>
-                <a href="{{ route("showUsers") }}" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Users</a>
-            </li>
-
-            <li class="pl-24">
-                <a href="{{ route("showRegister") }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</a>
+                <a href="{{ route("showRegister") }}" class="text-white">Register</a>
             </li>
         </ul>
     </div>
-    <form class="form-inline" action="{{ route('search') }}" method="GET">
+    <form class="form-inline mr-5" action="{{ route('search') }}" method="GET">
         <input class="form-control mr-sm-2 text-center" type="search" name="query" placeholder="Find a movie" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
--- resources/views/sessions/register.blade.php
+++ resources/views/sessions/register.blade.php
@@ -13,24 +13,24 @@
         </div>
     @endif
 
-    <main class="mx-36 my-12">
-        <form class="max-w-sm mx-auto" action="{{ route("handleRegister") }}" method="POST">
+    <main class="container d-flex align-items-center justify-content-center">
+        <form class="max-w-sm" action="{{ route("handleRegister") }}" method="POST">
             @csrf
 
-            <h1 class="text-3xl">Register here!</h1>
-            <div class="my-5">
-                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-                <input name="name" type="text" id="name" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="John Doe">
+            <h1 class="mt-5 mb-5 display-4 text-center text-primary">Register</h1>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input name="name" type="text" id="name" class="form-control" placeholder="John Doe">
             </div>
-            <div class="mb-5">
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                <input name="password" type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input name="password" type="password" id="password" class="form-control">
             </div>
-            <div class="mb-5">
-                <label for="repeat_password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
-                <input name="repeat_password" type="password" id="repeat_password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
+            <div class="form-group">
+                <label for="repeat_password">Repeat password</label>
+                <input name="repeat_password" type="password" id="repeat_password" class="form-control">
             </div>
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+            <button type="submit" class="btn btn-primary mt-3 mb-5">Register new account</button>
         </form>
     </main>
 @endsection
